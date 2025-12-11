9cb1af237751ca94d6d98af85afbf0a93b259a3c
Patience-Kori1 <koribiram@yahoo.fr>
2025-12-11 19:51:14 +0100

    feat(initialization): Project cleanup and setup of the architecture for the first component - Removed default generated React interface - Created  folder for reusable components - Created  folder dedicated to the Table component - Added a base  file with minimal structure

---
+++ src/components/TableComponent.jsx
@@ -0,0 +1,20 @@
+export default function TableComponent() {
+    return (
+        <>
+            <h3>Mon composant Table</h3>
+            <table >
+                <thead>
+                    <tr>
+                        <th>Hello</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Hello</td>
+                    </tr>
+                </tbody>
+            </table>
+            
+        </>
+    )
+}
