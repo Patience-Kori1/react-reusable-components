e7d3d02d5499e979179bc4b6496666a5bbaa0345
Patience-Kori1 <koribiram@yahoo.fr>
2025-12-22 15:57:43 +0100

    feat(table): Ajout du composant Thead et intégration dans TableComponent - Création du composant `Thead` pour encapsuler la balise `<thead>` - Import et utilisation du composant `TrowHead` dans `Thead` - Intégration du composant `Thead` dans `TableComponent` - Clarification de la hiérarchie des composants d’en-tête : TableComponent → Thead → TrowHead → TdataHead - Poursuite de la construction modulaire du composant Table réutilisable

--- src/components/tableComponent/TableComponent.jsx
+++ src/components/tableComponent/TableComponent.jsx
@@ -1,15 +1,14 @@
 import TrowBody from "./TrowBody"
 import TBody from "./Tbody"
 import TrowHead from "./TrowHead"
+import Thead from "./Thead"
 
 export default function TableComponent() {
     return (
         <>
             <h3>Mon composant Table</h3>
             <table >
-                <thead>
-                    <TrowHead/>
-                </thead>
+                <Thead/>
                 <TBody/>
             </table>
             
---
+++ src/components/tableComponent/Thead.jsx
@@ -0,0 +1,8 @@
+import TrowHead from "./TrowHead"
+export default function Thead() {
+    return(
+        <thead>
+            <TrowHead/>
+        </thead>
+    )
+}
