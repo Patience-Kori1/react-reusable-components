f6b870cb34b28c227d5e70f3fb2c05d7c90795ce
Patience-Kori1 <koribiram@yahoo.fr>
2026-01-23 23:55:35 +0100

    feat(TdataBodyAction) : Amélioration du default render

--- src/components/tableComponent/TableComponent.jsx
+++ src/components/tableComponent/TableComponent.jsx
@@ -39,13 +39,13 @@ export default function TableComponent() {
     //     </>
     // )
      // Fonction render pour la colonne Action
-  const divAction = (row) => (
-    <td>
-      <button onClick={() => handleEdit(row)}>Modifier</button>
-      <button onClick={() => handleDelete(row)}>Supprimer</button>
-      <button onClick={() => handleDelete(row)}>Supprimer</button>
-    </td>
-  )
+  // const divAction = (row) => (
+  //   <td>
+  //     <button onClick={() => handleEdit(row)}>Modifier</button>
+  //     <button onClick={() => handleDelete(row)}>Supprimer</button>
+  //     <button onClick={() => handleDelete(row)}>Supprimer</button>
+  //   </td>
+  // )
     return (
         <>
             <h3>Mon composant Table</h3>
@@ -55,7 +55,7 @@ export default function TableComponent() {
                 columns={columns}
                 onAction={handleAction}
                 onEdit={handleEdit}
-                divAction={divAction}
+                // divAction={divAction}
             />
             <Pagination/>
         </>
--- src/components/tableComponent/TdataBodyAction.jsx
+++ src/components/tableComponent/TdataBodyAction.jsx
@@ -1,17 +1,42 @@
 export default function TdataBodyAction({row, columns, onAction, onEdit, divAction}) {
     // console.log(divAction)
-     const divActionDefault = (
-         <>
-            <td>
-                <button
-                    type="button"
-                    onClick={() => onEdit(row)}
-                >
-                Modifier
-                </button>
-                <button key={row.id} type="button" onClick={() => onAction(row)}>Dire Bonjour</button>
-            </td>
-        </>
-     )
-    return divAction ? divAction(row) : divActionDefault
+    //  const divActionDefault = (
+    //      <>
+    //         <td>
+    //             <button
+    //                 type="button"
+    //                 onClick={() => onEdit(row)}
+    //             >
+    //             Modifier
+    //             </button>
+    //             <button key={row.id} type="button" onClick={() => onAction(row)}>Dire Bonjour</button>
+    //         </td>
+    //     </>
+    //  )
+    // return divAction ? divAction(row) : divActionDefault
+
+    const divActionDefault = ( <td>
+      <button
+        type="button"
+        onClick={() => onEdit && onEdit(keyAction)}
+      >
+        {/* {textButtonEditAction || " Modifier"} */}
+        Modifier
+      </button>
+
+      <button
+        type="button"
+        onClick={() => onDelete && onDelete(keyAction)}
+      >
+        Supprimer
+      </button>
+    </td>)
+
+    // S'il y a un props divAction dans App, on l'affiche sinon on affiche divActiondefault
+    const displayAction =  divAction !== undefined ? divAction : divActionDefault
+    // console.log(onDelete)
+  return (
+    displayAction
+  )
+
 }
